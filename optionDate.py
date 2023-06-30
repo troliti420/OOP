95ad17921757016bd6a8b82b350c7b7e429bd36c
Dan Puscuta <puscuta.dan08@gmail.com>
2023-06-30 16:01:19 +0200

    oops

--- optionDate.py
+++ optionDate.py
@@ -1,7 +1,8 @@
 class OptionDate:
-     def __init__(self, useDate, monthToWord, yearTwoChar, yearFourChar, frenchMonth):
+     def __init__(self, useDate, monthToWord, yearTwoChar, yearFourChar, frenchMonth, englishMonth):
         self.useDate = useDate
         self.monthToWord = monthToWord
         self.yearTwoChar = yearTwoChar
         self.yearFourChar = yearFourChar
         self.frenchMonth = frenchMonth
+        self.englishMonth = englishMonth
